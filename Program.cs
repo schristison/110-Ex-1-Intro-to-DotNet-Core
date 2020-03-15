8e712f151be65f06f97903148b28ac66ae56e36f
schristison <56772078+schristison@users.noreply.github.com>
2020-03-15 16:24:12 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Test();
+        }
+        static void Test()
+        {
+            Console.WriteLine("This is a method");
+
+            /* Variables and Data types */
+            string firstName = "Sandie";
+            Console.WriteLine("My name is: " + firstName);
+
+            int age = 99;
+            long seconds = 123456789123456789;
+
+            float price = 12.34f;
+            decimal price2 = 123.23m;  // usually decimals are used for money
+
+            bool isThisCrazy = true;
+
+            // if statements
+            if(isThisCrazy)
+            {
+                Console.WriteLine("Yeah it is");
+            }
+            else
+            {
+                Console.WriteLine("No it isn't");
+            }
+
+            // loops
+            for(int i=0; i<10; i++)
+            {
+                Console.WriteLine("Iteration: " + i);
+            }
+        }
+    }
+}
